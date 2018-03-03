0b3f7a437697626748b57f288a91cc42e73eeb19
Oleg Utitsyn <utitsyn@gmail.com>
2018-03-03 21:26:01 +0300

    Update order type

--- src/types/types.js
+++ src/types/types.js
@@ -25,17 +25,31 @@ export type Order = {
     | {
         eventType: 'message',
         userId: string,
+        login: string,
         text: string,
       }
     | {
         eventType: 'add to cart' | 'remove from cart',
         userId: string,
+        login: string,
         productId: string,
       }
     | {
         eventType: 'set pay sum' | 'increase pay sum' | 'decrease pay sum',
         userId: string,
+        login: string,
         paySum: number,
+      }
+    | {
+        eventType: 'order pay',
+        userId: string,
+        login: string,
+        paySum: number,
+      }
+    | {
+        eventType: 'cancel order',
+        userId: string,
+        login: string,
       },
   >,
 }
