fd691173ed3be4c3cbf363db204ac20e72232322
Pavel Tereschenko <me@stupidcat.wtf>
2018-03-03 17:02:44 +0300

    Add orders reducer

--- src/front/store/reducers/reducers.js
+++ src/front/store/reducers/reducers.js
@@ -2,18 +2,39 @@
 
 import { combineReducers } from 'redux'
 
+import * as actions from '../../../actions'
+import * as types from '../../../types'
+
 import {
   type State as SelfPosition,
   type State as MapCenter,
   position,
 } from './positionReducer'
 
+export type { SelfPosition, MapCenter }
+
 export type State = {
   selfPosition: SelfPosition,
   mapCenter: MapCenter,
 }
 
+export type Orders = $ReadOnlyArray<types.Order>
+
+export const orders = (
+  state: Orders = [],
+  action: actions.OrdersUpdate,
+): Orders => {
+  switch (action.type) {
+    case 'order update':
+      return action.payload
+
+    default:
+      return state
+  }
+}
+
 export const rootReducer = combineReducers({
   selfPosition: position('update self position'),
   mapCenter: position('set map center'),
+  orders,
 })
