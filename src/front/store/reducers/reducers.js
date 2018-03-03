09cc2533cecbd0d41d37fb5d435760b18148a3c6
Pavel Tereschenko <me@stupidcat.wtf>
2018-03-03 16:45:55 +0300

    Add position reducers

--- src/front/store/reducers/reducers.js
+++ src/front/store/reducers/reducers.js
@@ -2,6 +2,18 @@
 
 import { combineReducers } from 'redux'
 
+import {
+  type State as SelfPosition,
+  type State as MapCenter,
+  position,
+} from './positionReducer'
+
+export type State = {
+  selfPosition: SelfPosition,
+  mapCenter: MapCenter,
+}
+
 export const rootReducer = combineReducers({
-  empty: () => null,
+  selfPosition: position('update self position'),
+  mapCenter: position('set map center'),
 })
