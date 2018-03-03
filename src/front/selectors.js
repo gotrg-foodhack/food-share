b8ac9c47c87669c2de8ca11c75fccfe889aa3c65
Pavel Tereschenko <me@stupidcat.wtf>
2018-03-03 21:36:04 +0300

    Add getUserName and getMyOrderId selectors

--- src/front/selectors.js
+++ src/front/selectors.js
@@ -11,6 +11,10 @@ export const getUserId: (state: State) => * = compose(
   user => (user.status === 'logged in' ? user.id : undefined),
   getUser,
 )
+export const getUserName: (state: State) => * = compose(
+  user => (user.status === 'logged in' ? user.username : undefined),
+  getUser,
+)
 
 export const getMyOrder: (state: State) => * = compose(
   ({ orders, userId }) =>
@@ -19,6 +23,11 @@ export const getMyOrder: (state: State) => * = compose(
     orders: getOrders(state),
     userId: getUserId(state),
   }),
+)
+
+export const getMyOrderId: (state: State) => * = compose(
+  order => order && order.id,
+  getMyOrder,
 )
 
 export const isIOwner: (state: State) => * = compose(
