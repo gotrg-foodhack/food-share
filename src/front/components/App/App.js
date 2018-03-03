a44cc1ca3dea68276f837a6d27fd48b15b780c0b
Pavel Tereschenko <me@stupidcat.wtf>
2018-03-03 22:50:16 +0300

    Update App

--- src/front/components/App/App.js
+++ src/front/components/App/App.js
@@ -1,11 +1,23 @@
 /* @flow */
 
 import * as React from 'react'
+import { connect } from 'react-redux'
 
 import { Order } from '../Order'
 
-export const App = () => (
-  <React.Fragment>
-    <Order />
-  </React.Fragment>
-)
+import * as store from '../../store/reducers'
+import * as selectors from '../../selectors'
+
+import { Map } from '../Map'
+
+export const App = connect(
+  (state: store.State) => ({
+    isLoggedIn: selectors.isLoggedIn(state),
+    haveActiveOrder: selectors.haveActiveOrder(state),
+  }),
+  () => ({}),
+)(({ isLoggedIn, haveActiveOrder }) => {
+  if (!isLoggedIn) return null
+  if (haveActiveOrder) return <Order />
+  return <Map />
+})
