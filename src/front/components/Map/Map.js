a62894801d30b8b54c6bca527b1a4b93c2037948
Pavel Tereschenko <me@stupidcat.wtf>
2018-03-04 03:16:00 +0300

    Add avatars and join to order action support

--- src/front/components/Map/Map.js
+++ src/front/components/Map/Map.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import styled from 'styled-jss'
 import { connect } from 'react-redux'
-import ReactMapboxGl, { ZoomControl } from 'react-mapbox-gl'
+import ReactMapboxGl, { ZoomControl, Marker } from 'react-mapbox-gl'
 
 import { compose } from 'recompose'
 
@@ -12,6 +12,17 @@ import * as selectors from '../../selectors'
 import { type State } from '../../store/reducers'
 
 import { MapOverlay } from '../MapOverlay'
+
+import { UserAvatar } from './avatars'
+
+const AvatarButton = styled('button')({
+  padding: 0,
+  margin: 0,
+  border: 'none',
+  background: 'none',
+  borderRadius: '50%',
+  cursor: 'pointer',
+})
 
 const Wrapper = styled('div')({
   height: '100%',
@@ -27,8 +38,11 @@ export const Map = compose(
   connect(
     (state: State) => ({
       mapCenter: selectors.getMapCenterPoint(state),
+      orders: selectors.getOrders(state),
     }),
     dispatch => ({
+      joinToOrder: (orderId: string) => () =>
+        dispatch(actions.joinToOrder(orderId)),
       setMapCenter: ({ painter }) =>
         dispatch(
           actions.setMapCenter({
@@ -38,7 +52,7 @@ export const Map = compose(
         ),
     }),
   ),
-)(({ mapCenter, setMapCenter }) => (
+)(({ mapCenter, setMapCenter, orders, joinToOrder }) => (
   <Wrapper>
     <MapOverlay />
     <MapBox
@@ -49,6 +63,14 @@ export const Map = compose(
         height: '100%',
         width: '100%',
       }}>
+      {orders.map(order => (
+        <Marker key={order.id} coordinates={mapCenter} anchor="bottom">
+          <AvatarButton onClick={joinToOrder(order.id)}>
+            <UserAvatar order={order} />
+          </AvatarButton>
+        </Marker>
+      ))}
+
       <ZoomControl position="top-right" />
     </MapBox>
   </Wrapper>
